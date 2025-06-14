0f65474d27e8ea9131e737c9b86ad20c8b3fe955
= <dochyun123@gmail.com>
2025-06-14 21:03:56 +0900

    Refactor: Use function and use infinite loop

--- checkQR.py
+++ checkQR.py
@@ -1,30 +1,34 @@
 import pandas as pd
 
-wakeup_data = pd.read_csv("data.csv", encoding="utf-8")
+def checkQR(QRcode, wakeup_data):
+    wakeup_data
+    NotValid_msg = "유효하지 않은 QR코드입니다."
+    Welcome_msg = "입장완료되었습니다"
+    Duplicate_msg = "이미 입장완료된 QR코드입니다."
+    filtered_rows = wakeup_data.loc
+    [
+        wakeup_data["고유번호"] == QRcode
+    ]  # QR 코드 고유번호 찾기
 
-QRcode = "QEw5m3PQl0WKVEY255LUliYOJxYe24"
+    if filtered_rows.empty:
+        return NotValid_msg  # QR 코드가 존재하지 않으면 출력
 
-if "checked" not in wakeup_data.columns:
-    wakeup_data["checked"] = ""  # 기본값을 False로 설정
-
-
-filtered_rows = wakeup_data.loc[wakeup_data["고유번호"] == QRcode]
-
-
-# print(filtered_rows["checked"].values[0])
-
-if filtered_rows.empty:
-    print("유효하지 않은 QR코드입니다.")  # QR 코드가 존재하지 않으면 출력
-else:
-    checked_value = filtered_rows["checked"].values[0]
-
-    if pd.isna(checked_value) or checked_value == "":
+    elif (
+        pd.isna(filtered_rows["checked"].values[0])
+        or filtered_rows["checked"].values[0] == ""
+    ):
         wakeup_data.loc[wakeup_data["고유번호"] == QRcode, "checked"] = "입장완료"
-        print("입장완료되었습니다.")
-        name = wakeup_data.loc[wakeup_data["고유번호"] == QRcode, "이름"]
-        print(f"입장한 사람의 이름: {name.values[0]}")
+        return Welcome_msg
 
-    else:
-        print("이미 입장완료된 QR코드입니다.")
+    elif filtered_rows["checked"].values[0] == "입장완료":
+        return Duplicate_msg
 
-wakeup_data.to_csv("data.csv", index=False, encoding="utf-8")
+
+while True:
+    wakeup_data = pd.read_csv("data.csv", encoding="utf-8")
+    QRcode = input("QR코드를 입력하세요 (종료하려면 'exit' 입력): ")
+    if QRcode.lower() == "exit":
+        break
+    result = checkQR(QRcode, wakeup_data)
+    print(result)
+    wakeup_data.to_csv("data.csv", index=False, encoding="utf-8")
